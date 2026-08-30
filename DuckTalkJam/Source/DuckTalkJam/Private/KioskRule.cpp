3660cc4cb3cb8af0c98eb0f79d150fd28f95a723
ThaGoopa <waibryne@gmail.com>
2026-08-30 18:32:37 +0200

    Merge branch 'Shader-sperimentation' of https://github.com/Ducktalk-Games/DuckTalk-Jam into Shader-sperimentation

---
+++ DuckTalkJam/Source/DuckTalkJam/Private/KioskRule.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KioskRule.h"
+#include "KioskGameModeBase.h"
+
+bool UKioskRule::IsViolatedBy_Implementation(AKioskCharacter* Character) const
+{
+	return false;
+}
---
+++ DuckTalkJam/Source/DuckTalkJam/Public/KioskGameplayEvent.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "KioskGameplayEvent.generated.h"
+
+class AKioskGameModeBase;
+
+UCLASS(Abstract, Blueprintable)
+class DUCKTALKJAM_API UKioskGameplayEvent : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText DisplayName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText Description;
+
+	UFUNCTION(
+		BlueprintNativeEvent,
+		BlueprintCallable,
+		meta = (ToolTip = "Called when the event begins. Use this for presenting the event to the player, such as UI messages, phone calls, prompts, sounds, or other setup that tells the player what is happening.")
+	)
+	void StartEvent(AKioskGameModeBase* GameMode);
+
+	UFUNCTION(
+		BlueprintNativeEvent,
+		BlueprintCallable,
+		meta = (ToolTip = "Checks whether the gameplay condition for this event has been completed. Use this for actual game-state checks, such as whether the radio is off, an item was removed, or another required world condition is true.")
+	)
+	bool IsCompleted(AKioskGameModeBase* GameMode) const;
+
+	virtual void StartEvent_Implementation(AKioskGameModeBase* GameMode);
+	virtual bool IsCompleted_Implementation(AKioskGameModeBase* GameMode) const;
+};
