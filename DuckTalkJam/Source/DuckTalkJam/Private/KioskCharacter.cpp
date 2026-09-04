3c723d4468e6150295a488d81c6865a4efab70a5
Borna Hukman <borna@hukman.dev>
2026-09-04 13:53:34 +0200

    Added clothing components

--- DuckTalkJam/Source/DuckTalkJam/Private/KioskCharacter.cpp
+++ DuckTalkJam/Source/DuckTalkJam/Private/KioskCharacter.cpp
@@ -11,6 +11,44 @@ AKioskCharacter::AKioskCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Actor root
+	USceneComponent* Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	// Hat
+	HatRoot = CreateDefaultSubobject<USceneComponent>(TEXT("HatRoot"));
+	HatRoot->SetupAttachment(RootComponent);
+
+	HatMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HatMesh"));
+	HatMesh->SetupAttachment(HatRoot);
+
+	// Glasses
+	GlassesRoot = CreateDefaultSubobject<USceneComponent>(TEXT("GlassesRoot"));
+	GlassesRoot->SetupAttachment(RootComponent);
+
+	GlassesMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GlassesMesh"));
+	GlassesMesh->SetupAttachment(GlassesRoot);
+
+	// Beard
+	BeardRoot = CreateDefaultSubobject<USceneComponent>(TEXT("BeardRoot"));
+	BeardRoot->SetupAttachment(RootComponent);
+
+	BeardMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BeardMesh"));
+	BeardMesh->SetupAttachment(BeardRoot);
+
+	// Patch
+	PatchRoot = CreateDefaultSubobject<USceneComponent>(TEXT("PatchRoot"));
+	PatchRoot->SetupAttachment(RootComponent);
+
+	PatchMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PatchMesh"));
+	PatchMesh->SetupAttachment(PatchRoot);
+
+	// Tie
+	TieRoot = CreateDefaultSubobject<USceneComponent>(TEXT("TieRoot"));
+	TieRoot->SetupAttachment(RootComponent);
+
+	TieMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TieMesh"));
+	TieMesh->SetupAttachment(TieRoot);
 }
 
 // Called when the game starts or when spawned
--- DuckTalkJam/Source/DuckTalkJam/Public/KioskCharacter.h
+++ DuckTalkJam/Source/DuckTalkJam/Public/KioskCharacter.h
@@ -18,6 +18,48 @@ public:
 	// Sets default values for this character's properties
 	AKioskCharacter();
 
+#pragma region Clothing
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<USceneComponent> HatRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<UStaticMeshComponent> HatMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Clothing|Sockets")
+	FName HatSocket;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<USceneComponent> GlassesRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<UStaticMeshComponent> GlassesMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Clothing|Sockets")
+	FName GlassesSocket;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<USceneComponent> BeardRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<UStaticMeshComponent> BeardMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Clothing|Sockets")
+	FName BeardSocket;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<USceneComponent> PatchRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<UStaticMeshComponent> PatchMesh;
+	UPROPERTY(
+		EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category = "Clothing|Sockets"		
+	)
+	FName PatchSocket;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<USceneComponent> TieRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Clothing|Components")
+	TObjectPtr<UStaticMeshComponent> TieMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Clothing|Sockets")
+	FName TieSocket;
+#pragma endregion
+
 	UPROPERTY(BlueprintReadOnly, Category = "Player Reference")
 	TObjectPtr<APlayerBase> PlayerReference;
 
