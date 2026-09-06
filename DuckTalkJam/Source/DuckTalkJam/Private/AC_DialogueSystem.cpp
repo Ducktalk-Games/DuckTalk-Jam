5746dec0cf84db13cef2e5afd47776309eefcc6b
Borna Hukman <borna@hukman.dev>
2026-09-06 13:37:50 +0200

    Adding shop and gas station level

--- DuckTalkJam/Source/DuckTalkJam/Private/AC_DialogueSystem.cpp
+++ DuckTalkJam/Source/DuckTalkJam/Private/AC_DialogueSystem.cpp
@@ -80,8 +80,9 @@ void UAC_DialogueSystem::GetCurrentNode(FF_DialogueRow& OutNode)
 
 void UAC_DialogueSystem::EndDialogue()
 {
-	// MVP ONLY; VERY HACKY WAY
+	// Necessary for phone dialogues to work separately
 	OnDialogueEnded.Broadcast(CurrentNode && CurrentNode->b_IsPhoneCallDialogue);
+	
 	CurrentNode = nullptr;
 	DialogueTable = nullptr;
 	b_IsInDialogue = false;
