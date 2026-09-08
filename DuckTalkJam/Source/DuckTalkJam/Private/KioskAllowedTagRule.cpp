3901653ba64387b7ff64efcc84bef1cf0e08774a
Borna Hukman <borna@hukman.dev>
2026-09-08 18:10:06 +0200

    Added better tag rules

--- DuckTalkJam/Source/DuckTalkJam/Private/KioskAllowedTagRule.cpp
+++ DuckTalkJam/Source/DuckTalkJam/Private/KioskAllowedTagRule.cpp
@@ -1,14 +1,14 @@
 // Copyright (c) 2026 Borna Hukman. All Rights Reserved.
 
 
-#include "KioskTagRule.h"
+#include "KioskAllowedTagRule.h"
 #include "GameplayTagContainer.h"
 #include "KioskCharacter.h"
 
-bool UKioskTagRule::IsViolatedBy_Implementation(FGameplayTagContainer Traits) const
+bool UKioskAllowedTagRule::IsViolatedBy_Implementation(FGameplayTagContainer Traits) const
 {
 	if (Traits.IsEmpty()) return false;
 
-	const bool bViolated = Traits.HasTag(ForbiddenTag);
+	const bool bViolated = Traits.HasTag(AllowedTags);
 	return bViolated;
 }
